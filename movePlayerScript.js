bd3c621ef0aa7721fb4d1a11d3cee4fd929e8e6b
jaholt <holt.ja.halfways@gmail.com>
2018-10-15 16:05:13 +0100

    The documentation has been added

--- movePlayerScript.js
+++ movePlayerScript.js
@@ -1,5 +1,7 @@
 var storedKeyPress = 'a';
 
+
+// This function moves a html element across the screen. It can be have the keypress code and the html element entered as parameters. It then returns the storedKeyPress variable to prevent the same key being pressed twice.
 function moveLeg (keyPress, legChoice) {
     $(document).keydown(function(e){
         if(e.keyCode == keyPress && e.keyCode !== storedKeyPress) {
