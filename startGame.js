d92db64337aa02db33f68bbd3c501e9340ce921c
Max Taylor <max.mclaren.taylor@outlook.com>
2018-10-15 15:53:59 +0100

    named function is now outside of other function

--- startGame.js
+++ startGame.js
@@ -10,22 +10,23 @@ function countdown(number) {
     return number - 1
 }
 
+function createTimer() {
+    var timing = setInterval(function () {
+        if(time === 1){
+            clearInterval(timing)
+            timer.innerHTML = 'Go!!'
+        } else {
+            time = countdown(time)
+            timer.textContent = time
+        }
 
+    },1000)
+}
 
 function Timer() {
     time = 4
     var timer = document.getElementById("timer")
     createTimer()
-    function createTimer() {
-        var timing = setInterval(function () {
-            if(time === 1){
-                clearInterval(timing)
-                timer.innerHTML = 'Go!!'
-            } else {
-                time = countdown(time)
-                timer.textContent = time
-            }
 
-        },1000)
-    }
 }
+
