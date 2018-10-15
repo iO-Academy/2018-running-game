d8b2211d7c90545e58fb8d18e6cefbf7411b1cf2
Max Taylor <max.mclaren.taylor@outlook.com>
2018-10-15 14:29:44 +0100

    Me and Abdi have finished the first task and displayed it to the front end

---
+++ startGame.js
@@ -0,0 +1,29 @@
+document.getElementById("button").addEventListener('click', function(){
+    setTimeout(startGame, 3000)
+    Timer()
+})
+
+ function startGame() {
+ }
+
+function countdown(number) {
+    return number - 1
+}
+
+function Timer() {
+    time = 4
+    var timer = document.getElementById("timer")
+    createTimer()
+    function createTimer() {
+        var timing = setInterval(function () {
+            if(time === 1){
+                clearInterval(timing)
+                timer.innerHTML = 'Go!!'
+            } else {
+                time = countdown(time)
+                timer.textContent = time
+            }
+
+        },1000)
+    }
+}
