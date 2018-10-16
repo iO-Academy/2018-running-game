304ba6b8867b2f46e22c840668c0f5ab0caa01dc
jaholt <holt.ja.halfways@gmail.com>
2018-10-16 15:33:29 +0100

    Updated the move player scripts to work with the materials being provided. COmes with html file that is only useful to run current script but can be adapted to work on the actual game.

--- movePlayerScript.js
+++ movePlayerScript.js
@@ -1,18 +1,20 @@
-var storedKeyPress = 'a';
+var storedKeyPress = 65;
 
 // This function moves a html element across the screen.
-// It can be have the keypress code and the html element entered as parameters.
-// It then returns the storedKeyPress variable to prevent the same key being pressed twice.
-// Added the distance parameter to make the function more dynamic, has to be written in quotes like this: "+=12px" or will not work
-function moveLeg (keyPress, legChoice, distance) {
-    $(document).keydown(function(e){
-        if(e.keyCode == keyPress && e.keyCode !== storedKeyPress) {
-            $(legChoice).animate({left: distance}, 0.01, function(){
-                return storedKeyPress = e.keyCode
-            })
-        }
-    })
-}
+// For parameters it takes the event "e" from the event listener,
+// the distance you want the html element to move
+// and the id of the html element you want to move.
+// It changes the storedKeyPress variable to prevent the same key being pressed twice.
 
-moveLeg(65, "#leftLeg", "+=12px")
-moveLeg(68, "#rightLeg", "+=12px")
+$(document).keydown(function(e){
+    var moveDistance = "+=12px";
+    moveLeg(e, moveDistance, "#imageBlock")
+})
+
+function moveLeg(e, distance, htmlId) {
+    if ((e.keyCode == 65 && e.keyCode !== storedKeyPress) || (e.keyCode == 68 && e.keyCode !== storedKeyPress)) {
+        $(htmlId).animate({left: distance}, 0, function () {
+            storedKeyPress = e.keyCode
+        })
+    }
+}
