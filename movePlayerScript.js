7196a6bbf61f1bddbee5bdf2cc9b8e46c72aff24
Ariel Walker-Cochrane <arielwalkercochrane@gmail.com>
2018-10-17 11:46:00 +0100

    made function to display finish message

--- movePlayerScript.js
+++ movePlayerScript.js
@@ -9,6 +9,7 @@ $(document).keydown(function(e) {
     var positionOfMan = parseInt($("#imageBlock").css('left'))
     var positionOfFinish = parseInt($("#finishLine").css('left'))
     finishLine(positionOfMan, positionOfFinish)
+
 })
 
 // This function moves a html element across the screen.
@@ -27,12 +28,16 @@ function moveLeg(e, distance, htmlId) {
 // This function evaluates when the user reaches the finish line in the game
 // The position of finish and position of man parameters are variables in the keydown event listener
 // These can be changed in the event listener to work with the different html elements in the actual game
-// Please replace the console.log() statement with the appropriate finish celebration code
 function finishLine (positionOfMan, positionOfFinish) {
     if (positionOfMan >= positionOfFinish) {
-        console.log('finished YAAAY!')
         $("#imageBlock").fadeOut(2000)
+        setTimeout(displayFinishMessage, 2000)
+
     }
 }
 
-
+// This function displays a message when the user reaches the finish line in the game.
+// Can be used to display fanfare when made.
+function displayFinishMessage(){
+    $('#finishedBox').css({"visibility": "visible"});
+}
