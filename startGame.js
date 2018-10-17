26a053afafaaf6e048be72102c9c6cf94d354708
olivergate <oliver.kg2@gmail.com>
2018-10-17 11:22:31 +0100

    javascript functions documented and unecessarry functions fgot rid of and similar functions combined

--- startGame.js
+++ startGame.js
@@ -1,31 +1,16 @@
-
-
-
-function sectionStateOnStart (aboveSection,startButton,playScreen,gameBox){
+// This opens the gamescreen on click of the start button.
 $('#startButton').click(function () {
-    hideAboveSection (aboveSection)
-    hideStartButton (startButton)
-    showPlayScreen (playScreen)
-    increaseGameHeight (gameBox)
+    displayToggleNone ($('#aboveSection'))
+    displayToggleNone ($('#startButton'))
+    showPlayScreen ($('#playScreen'))
+    displayToggleNone($('.topSection'))
 })
 
+//Toggle display from block to none
+function displayToggleNone (section) {
+    section.css({display: "none"})
 }
-
-function hideAboveSection (aboveSection) {
-    aboveSection.css({display: "none"})
-}
-
-function hideStartButton (startButton) {
-    startButton.css({display: "none"})
-}
-
+//toggles the visibility of a screen from hidden to visible and brings to the front.
 function showPlayScreen (playScreen) {
     playScreen.css({visibility: "visible", zIndex: 1})
 }
-
-function increaseGameHeight (gameBox) {
-    gameBox.css({height: "670px"})
-}
-
-
-sectionStateOnStart($('#aboveSection'),$('#startButton'),$('#playScreen'),$('#gameBox'))
