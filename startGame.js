77bb80df6d213159f3fa057e6ef0052619d17c3d
olivergate <oliver.kg2@gmail.com>
2018-10-17 11:49:04 +0100

    line break removed

--- startGame.js
+++ startGame.js
@@ -1,16 +1,15 @@
 // This opens the gamescreen on click of the start button.
-$('#startButton').click(function () {
-    displayToggleNone ($('#aboveSection'))
-    displayToggleNone ($('#startButton'))
-    showPlayScreen ($('#playScreen'))
+$('#startButton').click(function(){
+    displayToggleNone($('#aboveSection'))
+    displayToggleNone($('#startButton'))
+    showPlayScreen($('#playScreen'))
     displayToggleNone($('.topSection'))
 })
-
 //Toggle display from block to none
-function displayToggleNone (section) {
+function displayToggleNone(section){
     section.css({display: "none"})
 }
-//toggles the visibility of a screen from hidden to visible and brings to the front.
-function showPlayScreen (playScreen) {
+//Toggles the visibility of a screen from hidden to visible and brings to the front.
+function showPlayScreen(playScreen){
     playScreen.css({visibility: "visible", zIndex: 1})
 }
