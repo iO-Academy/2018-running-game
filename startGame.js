7a844af4ba8fa631ad9d1e277923cba85ca93c8f
olivergate <oliver.kg2@gmail.com>
2018-10-17 12:10:29 +0100

    functionality simplified in startgame.js

--- startGame.js
+++ startGame.js
@@ -1,15 +1,12 @@
 // This opens the gamescreen on click of the start button.
 $('#startButton').click(function(){
-    displayToggleNone($('#aboveSection'))
-    displayToggleNone($('#startButton'))
-    showPlayScreen($('#playScreen'))
-    displayToggleNone($('.topSection'))
+    buildGame()
 })
-//Toggle display from block to none
-function displayToggleNone(section){
-    section.css({display: "none"})
-}
-//Toggles the visibility of a screen from hidden to visible and brings to the front.
-function showPlayScreen(playScreen){
-    playScreen.css({visibility: "visible", zIndex: 1})
+
+//function shows the game screen and hides elements that aren't required for the game screen.
+function buildGame(){
+    $('#aboveSection').css({display: "none"})
+    $('#startButton').css({display: "none"})
+    $('.topSection').css({display: "none"})
+    $('#playScreen').css({visibility: "visible", zIndex: 1})
 }
