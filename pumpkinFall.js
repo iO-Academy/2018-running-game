de109b46fc4ca7a5679cb25ec2e19952713f658a
Matt <matthew.tapley@hotmail.co.uk>
2018-10-18 15:43:37 +0100

    moved fanfare pumpkins up to fit game screen

--- pumpkinFall.js
+++ pumpkinFall.js
@@ -3,6 +3,6 @@ function pumpkinStuff(){
     $(".fallingPumpkin").each(function () {
         $('.fallingPumpkin').css({"display": "block"})
         var random = (Math.random() * 100)  * 50
-        $(this).animate({top: "718px"}, random)
+        $(this).animate({top: "680px"}, random)
     })
 }
