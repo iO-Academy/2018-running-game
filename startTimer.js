02abeb0cc1a8e2dc5327fd152e35f2eed2e9f66d
Abdi Abdiaziz <a.abdiaziz@hotmail.co.uk>
2018-10-18 13:34:13 +0100

    Made changes requested from code review (commented out code to be deleted)

--- startTimer.js
+++ startTimer.js
@@ -1,39 +1,4 @@
-// var startTimer = document.getElementById("timer")
-// var time = 0
-// setInterval(setTime, 1000)
-//
-// function setTime() {
-//     ++time
-//     startTimer.innerHTML = raceTimer(time % 60)
-// }
-//
-// function raceTimer(time) {
-//     var raceTime = time + ' s'
-//     if (raceTime.length < 4) {
-//         return '0' + raceTime
-//     } else {
-//         return raceTime
-//     }
-// }
-//
-// // var finishedGame = new CustomEvent('finishedRace', {
-// //     bubbles: true,
-// //     cancelable: true
-// // })
-// //
-// // document.getElementById('timer').addEventListener('finishedRace', function() {
-// //     var timer = document.getElementById('timer')
-// //     var time = document.getElementById('timer').textContent
-// //     var finishTime = document.createElement('p')
-// //     finishTime.textContent = 'Finish Time: ' + time + 's!!'
-// //     timer.appendChild(finishTime)
-// // })
-
-// var MatchTimer = document.getElementById("timer")
-
-//
 var time = 0
-
 
 function setTime() {
     var commenceTimer = document.getElementById("timer")
@@ -49,8 +14,6 @@ function raceTimer() {
         return raceTime
     }
 }
-
-
 
 function gameTimer() {
     if (character.attr("dataMoving") == 1) {
